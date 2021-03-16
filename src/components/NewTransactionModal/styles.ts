a2dfe233c9bea99afa02e5736e318d90a3d49751
Artur-17 <arturandrade40@hotmail.com>
2021-03-16 20:03:24 -0300

    dados da api

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+import { darken, transparentize } from 'polished'
+
 export const Container = styled.form`
     h2 {
         color: var(--text-title);
@@ -55,4 +57,46 @@ export const TransactionTypeContainer = styled.div`
     grid-template-columns: repeat(2, 1fr);
     gap: 0.5rem;
 
+`;
+
+interface RadioBoxProps {
+    isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#33CC95',
+    red: '#E52E4D',
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+
+    background: ${ (props) => props.isActive 
+    ? transparentize(0.9, colors[props.activeColor])
+    : 'transparent'};
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: border-color 0.2s;
+
+    &:hover {
+        border-color: ${darken(0.1, '#d7d7d7')};
+    }
+
+    img {
+        width: 28px;
+        height: 28px;
+    }
+
+    span {
+        display: inline-block;
+        margin-left: 1rem;
+        font-size: 1rem;
+        color: var(--text-title);
+    }
 `;
--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,11 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "../TransactionTable/styles";
 
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
+
 export function TransactionTable() {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+
     useEffect(() => {
         api.get('transactions')
-        .then(response => console.log(response.data))
+        .then(response => setTransactions(response.data.transactions))
+
     }, []);
 
     return (
@@ -21,18 +34,23 @@ export function TransactionTable() {
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">- R$500</td>
-                        <td>Retirada</td>
-                        <td>20/02/2021</td>
+                   { transactions.map(transaction => (
+                    <tr key={transaction.id}>
+                        <td>{transaction.title}</td>
+                        <td 
+                            className={transaction.type}>
+                                {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'    
+                                }).format(transaction.amount)}
+                        </td>
+                        <td>{transaction.category}</td>
+                        <td> 
+                            {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}
+                        </td>
                     </tr>
-                    <tr>
-                        <td >Salário</td>
-                        <td className="deposit">R$3.000</td>
-                        <td>Depósito</td>
-                        <td>20/02/2021</td>
-                    </tr>
+                   ))}
+                    
                 </tbody>
             </table>
         </Container>
